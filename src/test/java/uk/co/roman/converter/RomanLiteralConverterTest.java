9d0f9d48063d09ad92c2644d59ba12a3e6316a91
Nanda <lan9le@bosch.com>
2022-07-31 21:31:24 +0100

    upgraded junit 5 and added more tests

--- src/test/java/uk/co/roman/converter/RomanLiteralConverterTest.java
+++ src/test/java/uk/co/roman/converter/RomanLiteralConverterTest.java
@@ -1,7 +1,9 @@
 package uk.co.roman.converter;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,9 +15,11 @@ public class RomanLiteralConverterTest {
    void before(){
        romanLiteralConverter = new RomanLiteralConverter();
    }
-    @Test
-    void testToRomanNumber(){
-        final var result = romanLiteralConverter.toRomanNumber(20);
-        assertEquals("XX", result);
+    @ParameterizedTest(name = "#{index} - Run test with args={0}")
+    @CsvSource({"20, XX", "50, L", "45, XLIV", "100, C", "150, CL"})
+    void testToRomanNumber(int value, String romanNumber){
+        final var result = romanLiteralConverter.toRomanNumber(value);
+        assertEquals(romanNumber, result);
     }
+
 }
