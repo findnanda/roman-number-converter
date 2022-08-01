4a1cf07c0d560eeeb0d2efe634b80ad2807d2858
Nanda <lan9le@bosch.com>
2022-08-01 04:51:06 +0100

    refactored the class to move static constants in dictionary

--- src/main/java/uk/co/roman/converter/RomanLiteralConverter.java
+++ src/main/java/uk/co/roman/converter/RomanLiteralConverter.java
@@ -1,20 +1,10 @@
 package uk.co.roman.converter;
 
-import java.util.*;
-import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.Comparator;
 
-import static java.util.stream.Collectors.toMap;
+import static uk.co.roman.converter.RomanDictionary.*;
 
 public class RomanLiteralConverter {
-    private static final Map<Integer, String> ROMAN_PATTERN;
-    private static final List<String> ROMAN_NUMERALS;
-    private static final Map<String, Integer> INTEGER_ROMAN_PATTERN;
-    private static final Function<Integer, Integer> MOD_KEY = index -> IntStream.range(1, String.valueOf(index).length())
-            .map(i -> 10 * 1).reduce(1, (v1, v2) -> v1 * v2);
-    private static final BiFunction<Integer, Integer, String> APPENDER;
 
     public String toRomanNumber(int number) {
         //find num length
@@ -27,9 +17,9 @@ public class RomanLiteralConverter {
         //find the length here and identify each character
         while(index < romanNumber.length()){
             //find a two digits first
-            Integer val = INTEGER_ROMAN_PATTERN.get(romanNumber.substring(index, Math.min(index + 2, romanNumber.length())));
-            if(val == null){
-                retVal =  retVal + INTEGER_ROMAN_PATTERN.get(String.valueOf(romanNumber.charAt(index)));
+            int val = RomanDictionary.lookUpDictionary(romanNumber.substring(index, Math.min(index + 2, romanNumber.length())));
+            if(val == -1){
+                retVal =  retVal + RomanDictionary.lookUpDictionary(romanNumber.charAt(index));
                 index++;
             } else {
                 retVal = retVal + val;
@@ -67,30 +57,4 @@ public class RomanLiteralConverter {
     private String getSingleDigitRomanNumber(int index) {
         return index == 0 ? "" : ROMAN_NUMERALS.get(index - 1);
     }
-
-
-    static {
-        ROMAN_PATTERN = new HashMap<>();
-        ROMAN_PATTERN.put(1, "I");
-        ROMAN_PATTERN.put(4, "IV");
-        ROMAN_PATTERN.put(5, "V");
-        ROMAN_PATTERN.put(9, "IX");
-        ROMAN_PATTERN.put(10, "X");
-        ROMAN_PATTERN.put(40, "XL");
-        ROMAN_PATTERN.put(50, "L");
-        ROMAN_PATTERN.put(90, "XC");
-        ROMAN_PATTERN.put(100, "C");
-        ROMAN_PATTERN.put(400, "CD");
-        ROMAN_PATTERN.put(500, "D");
-        ROMAN_PATTERN.put(900, "CM");
-        ROMAN_PATTERN.put(1000, "M");
-        ROMAN_PATTERN.put(4000, "MV̅");
-        ROMAN_PATTERN.put(5000, "V̅");
-        ROMAN_NUMERALS = Arrays.asList("I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X");
-        INTEGER_ROMAN_PATTERN = ROMAN_PATTERN.entrySet().stream().collect(toMap(Map.Entry::getValue, Map.Entry::getKey));
-        APPENDER = (appendCount, modKey) -> IntStream.rangeClosed(1, appendCount)
-                .mapToObj(index -> ROMAN_PATTERN.get(modKey))
-                .reduce("", (v1, v2) -> v1 + v2);
-    }
-
 }
